6b0efd42951761eef99634cb724da45b15215dda
Eric <57112072+ekfitz@users.noreply.github.com>
2019-11-14 01:14:38 -0700

    Change layout and background
    Add fonts and menu icon

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
+var logo = document.getElementById("logo");
+logo.innerHTML = "t b g" ;
 var tt1 = document.getElementById("tt1");
-tt1.innerHTML = "The Baines Group";
+//tt1.innerHTML = "The Baines Group";
