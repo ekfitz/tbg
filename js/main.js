ed61d667b6c348b867168ca7d33fcf661591757d
ekfitz <57112072+ekfitz@users.noreply.github.com>
2019-11-14 10:26:07 -0700

    First launch
    Scripts only do styles right now

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
 var logo = document.getElementById("logo");
-logo.innerHTML = "t b g" ;
+var nav1 = document.getElementById("nav1");
+var nav2 = document.getElementById("nav2");
+var nav3 = document.getElementById("nav3");
+
+var nt1 = document.getElementById("nt1");
+var nt2 = document.getElementById("nt2");
+var nt3 = document.getElementById("nt3");
+
 var tt1 = document.getElementById("tt1");
-//tt1.innerHTML = "The Baines Group";
+
+var btxt1 = document.getElementById("btxt1");
+btxt1.innerHTML = "Start Today";
+
+logo.innerHTML = "t b g" ;
+
+nav1.style.top = "3%";
+nav1.style.left = "74%";
+nav1.style.opacity = "0.7";
+
+nav2.style.top = "3%";
+nav2.style.left = "81%";
+nav2.style.opacity = "0.7";
+
+
+nav3.style.top = "3%";
+nav3.style.left = "88%";
+nav3.style.opacity = "0.7";
+
+
+nt1.innerHTML = "Press";
+nt2.innerHTML = "Design";
+nt3.innerHTML = "Studio";
+
+
+tt1.innerHTML = "Something Motivational Right Here";
+tt2.innerHTML = "Maybe some smaller text here to describe what happens when you push the button. It can be in a different font";
